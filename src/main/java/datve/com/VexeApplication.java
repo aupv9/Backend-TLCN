5d14213db225a11f15cf2ccc86aea5b3314ead8a
AuPhan <aupv96@gmail.com>
2019-09-18 00:03:30 +0700

    deploy

--- src/main/java/datve/com/VexeApplication.java
+++ src/main/java/datve/com/VexeApplication.java
@@ -9,5 +9,13 @@ public class VexeApplication {
     public static void main(String[] args) {
         SpringApplication.run(VexeApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api").allowedOrigins("*");
+            }
+        };
+    }
 }
