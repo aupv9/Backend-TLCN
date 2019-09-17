81c581118f62223d4e97bd0af1e14611444c09f0
AuPhan <aupv96@gmail.com>
2019-09-17 23:53:02 +0700

    deploy

---
+++ src/main/java/datve/com/controller/VeController.java
@@ -0,0 +1,44 @@
+package datve.com.controller;
+
+
+import datve.com.model.Ve;
+import datve.com.service.VeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class VeController {
+
+    @Autowired
+    @Qualifier("veService")
+    VeServiceImpl veService;
+
+    @RequestMapping(value = "/api/Ve",method = RequestMethod.POST,produces = "application/json")
+    public ResponseEntity<Ve> addVe(@RequestBody Ve ve){
+        HttpHeaders headers=new HttpHeaders();
+        if(veService.addVe(ve)){
+            return  new ResponseEntity<Ve>(ve,headers, HttpStatus.OK);
+        }
+        return new ResponseEntity<Ve>(ve,headers, HttpStatus.NOT_EXTENDED);
+    }
+
+    @RequestMapping(value = "/api/VeUpdate",method = RequestMethod.POST,produces = "application/json")
+    public ResponseEntity<Ve> cancelVe(@RequestBody Ve ve){
+        HttpHeaders headers=new HttpHeaders();
+
+        if(veService.cancelVe(ve)){
+            headers.add("token-mess","OK");
+            return new ResponseEntity<Ve>(ve,headers, HttpStatus.OK);
+        }
+        return new ResponseEntity<Ve>(ve,headers, HttpStatus.NOT_MODIFIED);
+    }
+
+}
---
+++ src/main/java/datve/com/service/XeService.java
@@ -0,0 +1,13 @@
+package datve.com.service;
+
+import datve.com.model.Xe;
+
+import java.util.List;
+
+public interface XeService {
+
+     List<Xe> searchXe(int start, int end, String date);
+     boolean addXe(Xe xe);
+     boolean updateXe(Xe xe);
+     List<Xe> getS();
+}
