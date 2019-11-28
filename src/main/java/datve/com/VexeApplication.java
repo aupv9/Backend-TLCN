20fc6758dc4aae2034a8896ac3213110d9e280f3
AuPhan <aupv96@gmail.com>
2019-11-28 13:34:18 +0700

    update bk

--- src/main/java/datve/com/VexeApplication.java
+++ src/main/java/datve/com/VexeApplication.java
@@ -13,13 +13,5 @@ public class VexeApplication {
     public static void main(String[] args) {
         SpringApplication.run(VexeApplication.class, args);
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
-    }
+
 }
